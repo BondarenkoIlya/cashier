334d31f810a3818dcafa4d6b0e2eef4b6536005c
Home <bondarenko_elijah@mail.ru>
2017-04-12 21:41:52 +0600

    Correct product service

--- src/main/java/com/university/ilya/service/ProductService.java
+++ src/main/java/com/university/ilya/service/ProductService.java
@@ -5,22 +5,39 @@ import com.university.ilya.dao.jdbc.DaoFactory;
 import com.university.ilya.dao.jdbc.ProductDao;
 import com.university.ilya.model.Product;
 
+import java.util.List;
+
 /**
  * @author Ilya_Bondarenko
  */
 public class ProductService {
 
     public Product insertProduct(Product product) throws ServiceException {
-        try(DaoFactory daoFactory = new DaoFactory()) {
-           try {
-               ProductDao dao = daoFactory.getDao(ProductDao.class);
-               product = dao.create(product);
-           }catch (DaoException e){
-               throw new ServiceException("Cannot create product", e);
-           }
+        try (DaoFactory daoFactory = new DaoFactory()) {
+            try {
+                ProductDao dao = daoFactory.getDao(ProductDao.class);
+                product = dao.create(product);
+            } catch (DaoException e) {
+                throw new ServiceException("Cannot create product", e);
+            }
         } catch (DaoException e) {
-            throw new ServiceException("Cannot create dao factory",e);
+            throw new ServiceException("Cannot create dao factory", e);
         }
         return product;
+    }
+
+    public List<Product> getAllProducts() throws ServiceException {
+        List<Product> allProducts;
+        try (DaoFactory daoFactory = new DaoFactory()) {
+            try {
+                ProductDao dao = daoFactory.getDao(ProductDao.class);
+                allProducts = dao.findAll();
+            }catch (DaoException e){
+                throw new ServiceException("Cannot get all products", e);
+            }
+        } catch (DaoException e) {
+            throw new ServiceException("Cannot get create dao factory", e);
+        }
+        return allProducts;
     }
 }
