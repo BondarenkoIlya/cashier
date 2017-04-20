e76d36cb175a94cadc5e05a30ed1ac04451216ca
Home <bondarenko_elijah@mail.ru>
2017-04-21 01:09:08 +0600

    Added forward to admin page by pushing escape key

--- src/main/java/com/university/ilya/Main.java
+++ src/main/java/com/university/ilya/Main.java
@@ -1,13 +1,10 @@
 package com.university.ilya;
 
-import com.university.ilya.controller.IndexController;
+import com.university.ilya.manager.SceneManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
-import java.net.URL;
 
 /**
  * @author Ilya_Bondarenko
@@ -16,19 +13,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        URL resource = getClass().getResource("/view/index.fxml");
-
-        fxmlLoader.setLocation(resource);
-
-        Parent root = fxmlLoader.load();
-        IndexController controller = fxmlLoader.getController();
-        controller.setStage(primaryStage);
-        Scene indexScene = new Scene(root, 600, 400);
-        controller.setIndexScene(indexScene);
-        primaryStage.setTitle("Browser Desktop!");
-        primaryStage.setScene(indexScene);
-        primaryStage.show();
+        SceneManager.changeLocation(primaryStage, "/view/index.fxml");
+        primaryStage.addEventHandler( KeyEvent.KEY_PRESSED, event -> {
+            if (KeyCode.ESCAPE == event.getCode()) {
+                SceneManager.changeLocation(primaryStage, "/view/admin-page.fxml");
+            }
+        });
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/university/ilya/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.university.ilya.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController extends Controller {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
